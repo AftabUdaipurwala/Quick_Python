37aac25dd3e5ca86d38440ecf5fa5d33425ac8c2
AftabUdaipurwala <udaipurwalaa@gmail.com>
2022-06-02 11:08:05 +0530

    Learnt how to create variables and assign values to it, it can be done by single and multiple assigments togather as shown in the script below

---
+++ Python Scripts/Variable and Assignments.py
@@ -0,0 +1,15 @@
+# Single assignment
+name = 'Aftab'
+age = 30
+profession = 'Python Developer'
+
+print("Name is {0}, age is {1} and profession is {2}" .format(name, age, profession))
+
+
+# multiple assigments
+name,age, profession = 'Aftab Udaipurwala', 30.3,'Python Developer'
+print("Name is {0}, age is {1} and profession is {2}" .format(name, age, profession))
+
+# Same value to multiple variables
+Aftab = Ramesh = Joseph = 30.3
+print('Aftabs age is {} , Rameshs age is {} , josephs age is {}'.format(Aftab,Ramesh,Joseph))
