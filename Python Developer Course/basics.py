816dce8a2faea78f6959cc7b2bf63441a40abb02
AftabUdaipurwala <udaipurwalaa@gmail.com>
2022-06-23 17:23:05 +0530

    Starting with the new course, will learn about the lists, dictionaries and also about the input function

---
+++ Python Developer Course/basics.py
@@ -0,0 +1,6 @@
+store_address = ['Flat Street', '118', 'New York']
+pins = {'Mike': 1111, 'Joe': 1234, 'Jack': 2222}
+
+print(store_address[0], store_address[1])
+pin = int(input('Enter a pin of Employee'))
+print(pin)
