f1db87c8f31e5cd607aff25f18e63f976acee7d7
AftabUdaipurwala <udaipurwalaa@gmail.com>
2022-06-25 15:22:08 +0530

    Learnt about parameterized funcitons, default parameters and if else condition in functions

--- Python Developer Course/Basics/Functions.py
+++ Python Developer Course/Basics/Functions.py
@@ -39,3 +39,50 @@ def converter(original_unit, coeff):
 
 
 print(10, 0.3048)
+
+
+# Functions wiht Default parameters
+
+def converter(original_unit, coeff=0.20):
+    return original_unit * coeff
+
+
+# dont pass default parameters
+print(converter(10))
+print(converter(10, 0.3))
+
+# Weather Function (E)
+'''Create a function that converts Celsius degrees to Fahrenheit. 
+The formula to convert Celsius to Fahrenheit is F = C × 9/5 + 32.
+This is not an interactive exercise. 
+Write this function using Python like you normally do'''
+
+
+def weather(Celsius):
+    F = (Celsius * 9 / 5) + 32
+    return F
+
+
+print(weather(36.2))
+
+# Functions and If else
+'''In one of the previous exercises, we created this function:
+def string_length(mystring):    
+    return len(mystring)
+Calling the function with a string as the value for the argument mystring will return the length of that string.
+If an integer is passed as an argument value:
+string_length(10)
+that would generate an error since the len() function doesn't work for integers.
+Your duty is to modify the function so that when an integer is passed as input, the function should output a message like "Sorry integers don't have length."'''
+
+
+def lenofstr(string):
+    if type(string) == int:
+        return 'Sorry integers don\'t have length.'
+    elif type(string) != str:
+        return 'Sorry not a valid string type'
+    else:
+        return len(string)
+
+
+lenofstr('Aftab')
