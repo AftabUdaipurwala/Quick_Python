ef40d366d96dc732559473e3009dca604444d9e4
AftabUdaipurwala <udaipurwalaa@gmail.com>
2022-06-25 07:21:00 +0530

    Learnt about the basics and advance part of functions in python using Functions.py

---
+++ Python Developer Course/Basics/Functions.py
@@ -0,0 +1,33 @@
+# Basic function
+# importing date class from datetime module
+from datetime import date
+
+
+# creating the date object of today's date
+
+def age_calc(birthyear):
+    todays_date = date.today()
+    year = todays_date.year
+    age = year - birthyear
+    return age  # return the value, the returned value can be reused n number of times
+
+
+age_calc(1986)
+print(type(age_calc(1986)))
+
+
+# functions without arguements
+def printing():
+    print("Hello World")  # prints but doesnt return anything, hence the type of this function is none type
+
+
+printing()
+print(type(printing()))
+
+
+# calculate the length of the string
+def stringlencal(strings):
+    return len(strings)
+
+
+stringlencal('Aftab Udaipurwala')
