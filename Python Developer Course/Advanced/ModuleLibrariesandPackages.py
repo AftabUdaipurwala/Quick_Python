403ed8b73427267a8d626cf366bdd2e990765898
AftabUdaipurwala <udaipurwalaa@gmail.com>
2022-06-27 11:56:59 +0530

    Learnt about difference in modules, packages and libraries

---
+++ Python Developer Course/Advanced/ModuleLibrariesandPackages.py
@@ -0,0 +1,12 @@
+import os
+
+os.listdir()  # list all the files in the folder
+
+os.__file__  # gets the location of the os module
+
+# pip is also a library
+pip
+install
+glob2
+
+# difference between module library and packages is module is one single file like os.py, library is inbuilt packages and packages are for external 3rd party
